afe4f3a9850f1b05440592febe43d4832c726e0f
asadfgglie <ac121878@outlook.com>
2023-07-27 05:19:45 +0800

    voicevox-tts v0.0.0 first commit

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-"# voicevox-tts" 
+# voicevox-tts
+
+---
+voicevox tts for [oobabooga/text-generation-webui](https://github.com/oobabooga/text-generation-webui).
+
+# Install
+
+---
+1. Create conda environment following by [text-generation-webui#manual-installation-using-conda](https://github.com/oobabooga/text-generation-webui#manual-installation-using-conda)
+2. Clone this repository and `cd` into repository folder.
+```commandline
+git clone https://github.com/asadfgglie/voicevox-tts.git
+cd voicevox-tts
+```
+3. Activate `textgen` environment make from [text-generation-webui#manual-installation-using-conda](https://github.com/oobabooga/text-generation-webui#manual-installation-using-conda), and use pip install dependency
+```commandline
+conda activate textgen
+pip install -r requirements.txt
+```
+4. Install voicevox engine following by [VOICEVOX/voicevox_engine](https://github.com/VOICEVOX/voicevox_engine#%E3%83%80%E3%82%A6%E3%83%B3%E3%83%AD%E3%83%BC%E3%83%89)
+5. Start text-generation-webui and go to `Session` > `Install or update an extension`, and put https://github.com/asadfgglie/voicevox-tts in it.
+6. Press `ENTER` to install
+
+# How to use
+
+---
+1. Before enable this extension, you must start `voicevox_engine`, make sure it is running on default port `50021`.
+2. Enable this extension
+3. Select Speaker, style
+4. Chat with bot!
---
+++ script.py
@@ -0,0 +1,112 @@
+import asyncio
+import re
+from pathlib import Path
+
+import anyio
+import gradio as gr
+from deep_translator import GoogleTranslator
+from voicevox import Client, Style
+
+from modules import shared
+
+params: dict[str, None|bool|int|str] = {
+    'activate': True,
+    'selected_voice': None,
+    'autoplay': False,
+    'selected_style': None,
+    'speaker_id': None,
+    'interrogative_speak': True,
+    'translate': True
+}
+
+wav_idx = 0
+now_style: dict[str, Style]|None = None
+
+
+def remove_surrounded_chars(string):
+    # this expression matches to 'as few symbols as possible (0 upwards) between any asterisks' OR
+    # 'as few symbols as possible (0 upwards) between an asterisk and the end of the string'
+    return re.sub("\*[^\*]*?(\*|$)", '', string)
+
+
+async def tmp():
+    async with Client() as client:
+        temp = {}
+        for i in await client.fetch_speakers((await client.fetch_core_versions())[0]):
+            temp[i.name] = i
+    return temp
+speakers = asyncio.run(tmp())
+params['selected_voice'] = [i for i in speakers.keys()][0]
+params['selected_style'] = speakers[params['selected_voice']].styles[0].name
+params['speaker_id'] = speakers[params['selected_voice']].styles[0].id
+
+def update_style(speaker_name):
+    global now_style, speakers
+    now_style = {}
+    for i in speakers[speaker_name].styles:
+        now_style[i.name] = i
+
+    return gr.Dropdown.update(choices=[i for i in now_style.keys()], value=None)
+
+
+async def get_voice_bytes(speaker_id: int, text: str, interrogative_speak: bool):
+    async with Client() as client:
+        return await (await client.create_audio_query(text, speaker_id)).synthesis(speaker=speaker_id, enable_interrogative_upspeak=interrogative_speak)
+
+
+def ui():
+    with gr.Accordion("Setting", open=True):
+        # Gradio elements
+        with gr.Row():
+            activate = gr.Checkbox(value=params['activate'], label='Activate TTS')
+            autoplay = gr.Checkbox(value=params['autoplay'], label='Play TTS automatically')
+            translate = gr.Checkbox(value=params['translate'], label='Translate model output into Japanese for voicevox')
+
+        with gr.Row():
+            voice = gr.Dropdown(value=params['selected_voice'], choices=[i for i in speakers.keys()], label='TTS Voice')
+            style = gr.Dropdown(value=params['selected_style'], choices=[], label='Voice style')
+            interrogative_speak = gr.Checkbox(value=params['interrogative_speak'], label='interrogative speak')
+
+    if shared.is_chat():
+        pass
+
+    # Event functions to update the parameters in the backend
+    activate.change(lambda x: params.update({'activate': x}), activate, None)
+    voice.change(lambda x: params.update({'selected_voice': x, 'selected_style': None, 'speaker_id': None}), voice, None).then(update_style, voice, style)
+    autoplay.change(lambda x: params.update({"autoplay": x}), autoplay, None)
+    style.change(lambda x: params.update({'selected_style': x, 'speaker_id': now_style[x].id if not (x is None) else None}), style, None)
+    interrogative_speak.change(lambda  x: params.update({'interrogative_speak': x}), interrogative_speak, None)
+    translate.change(lambda x: params.update({'translate': x}), translate, None)
+
+def output_modifier(string, state):
+    global params, wav_idx
+
+    if not params['activate'] or params['speaker_id'] is None:
+        print(string)
+        return string
+
+    original_string = string
+    string = remove_surrounded_chars(string)
+    string = string.replace('"', '')
+    string = string.replace('“', '')
+    string = string.replace('\n', ' ')
+    string = string.strip()
+    if string == '':
+        string = 'empty reply, try regenerating'
+
+    output_file = Path(f'extensions/voicevox_tts/outputs/{wav_idx:06d}.mp3')
+    print(f'Outputting audio to {str(output_file)}')
+    if params['translate']:
+        string = GoogleTranslator(target='ja').translate(string)
+    audio = anyio.run(get_voice_bytes, params['speaker_id'], string, params['interrogative_speak'])
+    with open(str(output_file), 'wb') as f:
+        f.write(audio)
+
+    autoplay = 'autoplay' if params['autoplay'] else ''
+    string = f'<audio src="file/{output_file.as_posix()}" controls {autoplay}></audio>'
+    wav_idx += 1
+
+    string += f'\n\n{original_string}'
+
+    shared.processing_message = "*Is typing...*"
+    return string
